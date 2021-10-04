712e8da8f095817e9cb631c141fdeeba848a84a3
miafelic <60333709+miafelic@users.noreply.github.com>
2021-10-04 15:32:36 -0400

    Create AND.vhdl

---
+++ Assignment 2/AND.vhdl
@@ -0,0 +1,9 @@
+entity AND2 is
+     port (in1, in2: in std_logic;
+     out1: out std_logic);
+end AND2;
+ 
+architecture behavioral_2 of AND2 is
+begin
+     out1 <= in1 and in2;
+end behavioral_2;
