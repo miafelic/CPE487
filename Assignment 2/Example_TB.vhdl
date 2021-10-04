9b197bbea6eec5e45314c24ce653c130d1b3737a
miafelic <60333709+miafelic@users.noreply.github.com>
2021-10-04 19:08:45 -0400

    Update Example_TB.vhdl

--- Assignment 2/Example_TB.vhdl
+++ Assignment 2/Example_TB.vhdl
@@ -1,56 +1,25 @@
 -- cite: https://vhdlguide.readthedocs.io/en/latest/vhdl/testbench.html
--- half_adder_process_tb.vhd
+-- half_adder_simple_tb.vhd
 
 library ieee;
 use ieee.std_logic_1164.all;
 
 
-entity half_adder_process_tb is
-end half_adder_process_tb;
+entity half_adder_simple_tb is
+end half_adder_simple_tb;
 
-architecture tb of half_adder_process_tb is
-    signal a, b : std_logic;
-    signal sum, carry : std_logic;
+architecture tb of half_adder_simple_tb is
+    signal a, b : std_logic;  -- inputs 
+    signal sum, carry : std_logic;  -- outputs
 begin
     -- connecting testbench signals with half_adder.vhd
     UUT : entity work.half_adder port map (a => a, b => b, sum => sum, carry => carry);
 
-    tb1 : process
-        constant period: time := 20 ns;
-        begin
-            a <= '0';
-            b <= '0';
-            wait for period;
-            assert ((sum = '0') and (carry = '0'))  -- expected output
-            -- error will be reported if sum or carry is not 0
-            report "test failed for input combination 00" severity error;
-
-            a <= '0';
-            b <= '1';
-            wait for period;
-            assert ((sum = '1') and (carry = '0'))
-            report "test failed for input combination 01" severity error;
-
-            a <= '1';
-            b <= '0';
-            wait for period;
-            assert ((sum = '1') and (carry = '0'))
-            report "test failed for input combination 10" severity error;
-
-            a <= '1';
-            b <= '1';
-            wait for period;
-            assert ((sum = '0') and (carry = '1'))
-            report "test failed for input combination 11" severity error;
-
-            -- Fail test
-            a <= '0';
-            b <= '1';
-            wait for period;
-            assert ((sum = '0') and (carry = '1'))
-            report "test failed for input combination 01 (fail test)" severity error;
-
-
-            wait; -- indefinitely suspend process
-        end process;
-end tb;
+    -- inputs
+    -- 00 at 0 ns
+    -- 01 at 20 ns, as b is 0 at 20 ns and a is changed to 1 at 20 ns
+    -- 10 at 40 ns
+    -- 11 at 60 ns
+    a <= '0', '1' after 20 ns, '0' after 40 ns, '1' after 60 ns;
+    b <= '0', '1' after 40 ns;        
+end tb ;
